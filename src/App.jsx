f426bdd13d28d1d4a0647498cec2df9a41a218d0
gregorylira <gregory.lira007@gmail.com>
2022-04-25 10:27:49 -0300

    🌟 feat: login page and finish basic

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import "./app.scss";
 import Home from "./pages/home";
 import Watch from "./pages/watch";
 import Register from "./pages/register";
+import Login from "./pages/login";
 
 const App = () => {
-  return <Register />;
+  return <Login />;
 };
 
 export default App;
